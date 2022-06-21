7af3b13f01c1bf64d36e76f1497e26b510662367
Juan Daza <jdazauribe@gmail.com>
2022-06-20 21:57:14 -0500

    la beta

---
+++ main.js
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let balotoCompras = prompt("cuantos baloto quiere comprar");
+let balotoValor = 12;
+let balotoTotal = balotoCompras * balotoValor;
+alert("El valor total de la compra es "+ balotoTotal);
+let dinero = prompt("cuanto dinero tiene");
+
+
+if (dinero >= 12 && dinero < 24){
+    alert("felicidades tenga su baloto, su vuelto es de " + (dinero - balotoTotal) + ", desea dejar propina?");
+
+}
+else if(dinero >= 24 && dinero < 36) {
+    alert("felicidades tenga sus 2 balotos, su vuelto es de " + (dinero - balotoTotal) + ", desea dejar propina?");
+}
+else if(dinero >= 36) {
+    alert("felicidades tenga sus 3 balotos, su vuelto es de " + (dinero - balotoTotal) + ", desea dejar propina?");
+    alert("gracias por dejar propina ;u")
+}
+*/
+
+/*
+let dineroElGordoTrokzas = prompt("cuanto dinero tienes robeto?");
+
+let heladoAgua = 0.6;
+let heladoCrema = 1.0;
+let heladix = 1.6;
+let heladovich = 1.7;
+let helardo = 1.8;
+let heladoConfites = 2.9;
+let poteUnCuarto = 2.9;
+
+
+if (dineroElGordoTrokzas >= 2.9){
+    alert("puedes comprar pote de 1/4 kg, y/o tambien potecito de helado con confites");
+    alert("si comprase su saldo queda en " + (dineroElGordoTrokzas - poteUnCuarto))
+}
+else if( dineroElGordoTrokzas >= 1.8 && dineroElGordoTrokzas < 2.9){
+    alert("helardo");
+    alert("si comprase su saldo queda en " + (dineroElGordoTrokzas - helardo))
+}
+else if( dineroElGordoTrokzas >= 1.7 && dineroElGordoTrokzas < 1.8){
+    alert("heladovich");
+    alert("si comprase su saldo queda en " + (dineroElGordoTrokzas - heladovich))
+}
+else if( dineroElGordoTrokzas >= 1.6  && dineroElGordoTrokzas < 1.7){
+    alert("heladix");
+    alert("si comprase su saldo queda en " + (dineroElGordoTrokzas - heladix))
+}
+else if(dineroElGordoTrokzas >= 1.0 && dineroElGordoTrokzas < 1.6){
+    alert("helado de crema");
+    alert("si comprase su saldo queda en " + (dineroElGordoTrokzas - heladoCrema))
+} 
+else if( dineroElGordoTrokzas >= 0.6 && dineroElGordoTrokzas < 1.0){
+    alert("helado de agua");
+    alert("si comprase su saldo queda en " + (dineroElGordoTrokzas - heladoAgua))
+}
+*/
